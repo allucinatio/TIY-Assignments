9de980ebd4d7bfbdd0979b2aac483623fa35f2f0
allucinatio <allucinatio@gmail.com>
2017-06-07 10:10:38 -0400

    Add fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+//fizzbuzz
+
+function sum(num) {
+  for (i = 0; i<= num; i++) {
+    if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+sum(100);
